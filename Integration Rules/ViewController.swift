8aa78574a976ba1aeebc7aa07462045314dca2ca
Rishi Anand <rranand@iu.edu>
2019-05-27 21:16:28 -0400

    Initial Commit

---
+++ Integration Rules/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Integration Rules
+//
+//  Created by Rishi Anand on 5/27/19.
+//  Copyright © 2019 Rishi Anand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
